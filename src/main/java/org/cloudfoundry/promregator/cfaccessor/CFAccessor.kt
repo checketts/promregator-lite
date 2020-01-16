4b6494357b02783a6fca4274c325470c258a69d4
Clint Checketts <clintchecketts@churchofjesuschrist.org>
2020-01-16 10:22:32 -0700

    Use properties to disable CFAccessor

--- src/main/java/org/cloudfoundry/promregator/cfaccessor/CFAccessor.kt
+++ src/main/java/org/cloudfoundry/promregator/cfaccessor/CFAccessor.kt
@@ -27,6 +27,7 @@ import org.cloudfoundry.reactor.tokenprovider.PasswordGrantTokenProvider
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
@@ -40,7 +41,7 @@ import javax.annotation.PostConstruct
 private val logger = KotlinLogging.logger {  }
 
 @Service
-@ConditionalOnMissingBean(CFAccessor::class)
+@ConditionalOnProperty("cf.api.enabled", matchIfMissing = true)
 class CFAccessor(
         val cf: CloudFoundryConfiguration,
         promregatorConfiguration: PromregatorConfiguration
--- src/test/java/org/cloudfoundry/promregator/promregator/PromregatorApplicationTests.kt
+++ src/test/java/org/cloudfoundry/promregator/promregator/PromregatorApplicationTests.kt
@@ -2,8 +2,10 @@ package org.cloudfoundry.promregator.promregator
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PromregatorApplicationTests {
 
 	@Test
